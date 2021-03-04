74345447e37ac687f3cf96500b54eb5b865cfff4
L3p1989 <phogan110@live.com>
2021-03-03 22:22:47 -0700

    created hover affect for #rndmTxt that turns bg-color red and border-radius to 4px

--- app.js
+++ app.js
@@ -7,11 +7,16 @@ btnSubmit.attr('disabled', true);
 //enable btnSubmit on keyup
 inputTxt.on('keyup', () => {
     btnSubmit.attr('disabled', false);
-})
+});
 //on click prevent clearing of form, alert current value of inputTxt, clear that value, and then disable btnSubmit
 btnSubmit.on('click', (e) => {
     e.preventDefault();
-    myDiv.append(`<h2>${inputTxt.val()}</h2>`);
+    myDiv.append(`<h2 id="rndmTxt">${inputTxt.val()}</h2>`).hover(() => {
+        $('#rndmTxt').css('background-color', "red");
+        $('#rndmTxt').css('border-radius', '4px')
+    }, () => {
+        $('#rndmTxt').css('background-color', 'white');
+    });
     inputTxt.val('');
     btnSubmit.attr('disabled', true);
 });
