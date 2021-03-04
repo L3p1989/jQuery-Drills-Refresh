aad28e6e88a3bba6fa67e23e8e47862324717d80
L3p1989 <phogan110@live.com>
2021-03-03 19:39:01 -0700

    created const for btnSubmit, and inputTxt. made btnSubmit disabled by default. created keyup function on inputTxt to enable btnSubmit. created click function that will prevent form from clearing on submit, alerts the value typed in inputTxt, clear the value of inputTxt, and disable btnSubmit

--- app.js
+++ app.js
@@ -1,4 +1,17 @@
-$('#btnSubmit').on('click', (e) => {
+//grab #btnSubmit
+const btnSubmit = $('#btnSubmit');
+//grab #inputTxt
+const inputTxt = $('#inputTxt');
+//disable btnSubmit by default
+btnSubmit.attr('disabled', true);
+//enable btnSubmit on keyup
+inputTxt.on('keyup', () => {
+    btnSubmit.attr('disabled', false);
+})
+
+btnSubmit.on('click', (e) => {
     e.preventDefault();
-    alert($('#inputTxt').val());
+    alert(inputTxt.val());
+    inputTxt.val('');
+    btnSubmit.attr('disabled', true);
 });
