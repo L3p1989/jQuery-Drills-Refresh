73abf484128501b56dfcb7b09616716a54675f45
L3p1989 <phogan110@live.com>
2021-03-03 23:17:17 -0700

    commented hover effect. created ul. modified click listener for btnSubmit to add li to ul. created click event listener for li to change text color. created dblclick even listener for li to remove li

--- app.js
+++ app.js
@@ -8,15 +8,31 @@ btnSubmit.attr('disabled', true);
 inputTxt.on('keyup', () => {
     btnSubmit.attr('disabled', false);
 });
+//random color generator
+function randomColor() {
+    const r = Math.floor(Math.random() * 256);
+    const g = Math.floor(Math.random() * 256);
+    const b = Math.floor(Math.random() * 256);
+    return `rgb(${r}, ${g}, ${b})`;
+};
 //on click prevent clearing of form, alert current value of inputTxt, clear that value, and then disable btnSubmit
 btnSubmit.on('click', (e) => {
     e.preventDefault();
-    myDiv.append(`<h2 id="rndmTxt">${inputTxt.val()}</h2>`).hover(() => {
-        $('#rndmTxt').css('background-color', "red");
-        $('#rndmTxt').css('border-radius', '4px')
-    }, () => {
-        $('#rndmTxt').css('background-color', 'white');
-    });
+    //myDiv.append(`<h2 id="rndmTxt">${inputTxt.val()}</h2>`).hover(() => {
+    //    $('#rndmTxt').css('background-color', "red");
+    //    $('#rndmTxt').css('border-radius', '4px')
+    //}, () => {
+    //    $('#rndmTxt').css('background-color', 'white');
+    //});
+    let li = $('<li></li>')
+    thisLst.append(li.text(`${inputTxt.val()}`)
+    .click(() => {
+        li.css('color', `${randomColor()}`)
+    })
+    .dblclick(() => {
+        li.remove()
+    })
+    )
     inputTxt.val('');
     btnSubmit.attr('disabled', true);
 });
@@ -24,3 +40,7 @@ btnSubmit.on('click', (e) => {
 $('body').append('<div id="myDiv"></div>');
 //Grab myDiv
 const myDiv = $('#myDiv');
+//create ul
+$('body').append('<ul id="thisLst"></ul>');
+//grab #thisLst
+const thisLst = $('#thisLst');
